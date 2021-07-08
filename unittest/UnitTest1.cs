207b936cea64482adbaffcadb50be30328682a4d
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-07-08 23:39:34 +0530

    Update UnitTest1.cs

--- unittest/UnitTest1.cs
+++ unittest/UnitTest1.cs
@@ -12,8 +12,8 @@ namespace unittest
    
             
                 //Max Number at 1st Position
-                float firstValue = 30.5f, secondValue = 20.5f, thirdValue = 10.5f;
-                float expected = 30.5f;
+                float firstValue = 25.5f, secondValue = 30.5f, thirdValue = 40.5f;
+                float expected = 40.5f;
                 FindMaximum find = new FindMaximum();
                 float actual = find.MaximumValue(firstValue, secondValue, thirdValue);
                 Assert.AreEqual(expected, actual);
@@ -22,8 +22,8 @@ namespace unittest
             public void TestMethod2()
             {
                 //Max Number at 2nd Position
-                float firstValue = 20.5f, secondValue = 30.5f, thirdValue = 10.5f;
-                float expected = 30.5f;
+                float firstValue = 25.5f, secondValue = 30.5f, thirdValue = 40.5f;
+                float expected = 40.5f;
                 FindMaximum find = new FindMaximum();
                 float actual = find.MaximumValue(firstValue, secondValue, thirdValue);
                 Assert.AreEqual(expected, actual);
@@ -32,8 +32,8 @@ namespace unittest
             public void TestMethod3()
             {
                 //Max Number at 3rd Position
-                float firstValue = 20.5f, secondValue = 10.5f, thirdValue = 30.5f;
-                float expected = 30.5f;
+                float firstValue = 25.5f, secondValue = 30.5f, thirdValue = 40.5f;
+                float expected = 40.5f;
                 FindMaximum find = new FindMaximum();
                 float actual = find.MaximumValue(firstValue, secondValue, thirdValue);
                 Assert.AreEqual(expected, actual);
